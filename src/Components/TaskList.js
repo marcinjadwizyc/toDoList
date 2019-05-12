b104b522132004cce8621a003fa6a0b7f2955ca3
BillyPilgrim12 <jadwizycmarcin@gmail.com>
2019-05-12 14:38:19 +0200

    Remove animations & refactor TaskList component

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -3,35 +3,25 @@ import React from "react";
 import Task from "./Task";
 
 const TaskList = (props) => {
-	let tasks;
+	const styles = props.doneList ? "task--done" : "";
 
-	if (props.doneList === false) {
-		tasks = props.tasksData.map((task) => {
-			if (task.done === false) {
-				return (
-					<Task key={task.id} taskID={task.id} removeFunc={props.removeFunc} doneFunc={props.doneFunc}>
-						{task.taskValue}
-					</Task>
-				);
-			}
-		});
-	} else if (props.doneList) {
-		tasks = props.tasksData.map((task) => {
-			if (task.done) {
-				return (
-					<Task
-						styles="task--done"
-						key={task.id}
-						taskID={task.id}
-						removeFunc={props.removeFunc}
-						doneFunc={props.doneFunc}
-					>
-						{task.taskValue}
-					</Task>
-				);
-			}
-		});
-	}
+	const data = props.tasksData.filter((task) => {
+		return task.done === props.doneList;
+	});
+
+	const tasks = data.map((task) => {
+		return (
+			<Task
+				styles={styles}
+				key={task.id}
+				taskID={task.id}
+				removeFunc={props.removeFunc}
+				doneFunc={props.doneFunc}
+			>
+				{task.taskValue}
+			</Task>
+		);
+	});
 
 	return <ul className="taskList">{tasks}</ul>;
 };
