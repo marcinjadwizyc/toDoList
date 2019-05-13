5855e679e2992c91661abf55e6da448282812fd8
BillyPilgrim12 <jadwizycmarcin@gmail.com>
2019-05-13 20:10:16 +0200

    Change title of priority icon based on priority value of task

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -3,8 +3,11 @@ import React from "react";
 const Task = (props) => {
 	let styles = [ "task", props.styles ];
 
+	let priorityTitle = "Add priority";
+
 	if (props.taskPriority) {
 		styles.push("task--priority");
+		priorityTitle = "Remove priority";
 	}
 
 	styles = styles.join(" ");
@@ -14,7 +17,7 @@ const Task = (props) => {
 			{props.children}
 			<div className="task__container-icon">
 				<i className="task__icon fas fa-trash-alt" onClick={props.removeTaskFunc} title="Remove task" />
-				<i className="task__icon fas fa-star" onClick={props.priorityTaskFunc} title="Mark task as priority" />
+				<i className="task__icon fas fa-star" onClick={props.priorityTaskFunc} title={priorityTitle} />
 				<i className="task__icon fas fa-check" onClick={props.doneTaskFunc} title="Mark task as done" />
 			</div>
 		</li>
