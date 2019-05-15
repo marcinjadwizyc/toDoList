5178630ca24f1fda5492aade06f54e7f6e9e05c5
BillyPilgrim12 <jadwizycmarcin@gmail.com>
2019-05-15 20:58:04 +0200

    Rebuild task

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -14,12 +14,20 @@ const Task = (props) => {
 
 	return (
 		<li className={styles} id={props.taskID}>
-			{props.children}
-			<div className="task__container-icon">
-				<i className="task__icon fas fa-trash-alt" onClick={props.removeTaskFunc} title="Remove task" />
-				<i className="task__icon fas fa-star" onClick={props.priorityTaskFunc} title={priorityTitle} />
-				<i className="task__icon fas fa-check" onClick={props.doneTaskFunc} title="Mark task as done" />
+			<div className="task__main">
+				{props.children}
+				<div className="task__icons">
+					<i className="task__icon fas fa-trash-alt" onClick={props.removeTaskFunc} title="Remove task" />
+					<i className="task__icon fas fa-star" onClick={props.priorityTaskFunc} title={priorityTitle} />
+					<i className="task__icon fas fa-check" onClick={props.doneTaskFunc} title="Mark task as done" />
+				</div>
 			</div>
+			{false ? (
+				<div className="task__extention">
+					<input className="task__title-input" type="text" />
+					<textarea className="task__description-input">Describe your task here...</textarea>
+				</div>
+			) : null}
 		</li>
 	);
 };
